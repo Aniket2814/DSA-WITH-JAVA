940d676b4a151f4a1094c68fbcd822af95cf6954
Aniket Rathore <aniketrathore911@gmail.com>
2025-04-30 22:56:17 +0530

    Largest Element in an Array Brute approach

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+// Largest Element in an Array
+import java.util.*;
+class Demo1
+{
+public static void main(String ar[])
+{
+int x[]={3,2,1,5,2};
+Arrays.sort(x);
+System.out.println("The Largest Element is " +(x[x.length-1]));
+}
+
+}
