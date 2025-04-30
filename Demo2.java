e73c9cb280e2fc8d276c142e924750f80c556078
Aniket Rathore <aniketrathore911@gmail.com>
2025-04-30 23:07:03 +0530

    Second Largest Element in an Array Optimal approach

--- Demo2.java
+++ Demo2.java
@@ -6,15 +6,21 @@ public static void main(String ar[])
 {
 int x[]={1,2,4,7,7,5};
 int n=x.length;
-Arrays.sort(x);
-int largest=x[n-1];
-int secondlargest=-1;
-for(int i=n-2;i>=0;i--)
+
+int largest=x[0];
+for(int i=1;i<n;i++)
 {
-	if(x[i]!=largest)
+	if(x[i]>largest)
+	{
+		largest=x[i];
+	}
+}
+int secondlargest=-1;
+for(int i=0;i<n;i++)
+{
+	if(x[i]>secondlargest && x[i]<largest)
 	{
 		secondlargest=x[i];
-		break;
 	}
 }
 System.out.println("The Second Largest Element is " +secondlargest);
