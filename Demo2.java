9478e06f70cc621ad2e9ff50efb03d78d92d66c6
Aniket Rathore <aniketrathore911@gmail.com>
2025-04-30 23:09:36 +0530

    Second Largest Element in an Array Better approach

--- Demo2.java
+++ Demo2.java
@@ -8,17 +8,15 @@ int x[]={1,2,4,7,7,5};
 int n=x.length;
 
 int largest=x[0];
-for(int i=1;i<n;i++)
-{
-	if(x[i]>largest)
-	{
-		largest=x[i];
-	}
-}
 int secondlargest=-1;
 for(int i=0;i<n;i++)
 {
-	if(x[i]>secondlargest && x[i]<largest)
+	if(x[i]>largest)
+	{
+		secondlargest=largest;
+		largest=x[i];
+	}
+	else if(x[i]>secondlargest && x[i]<largest)
 	{
 		secondlargest=x[i];
 	}
