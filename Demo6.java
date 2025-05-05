8106724d36d6d9639813e5a8f94cfe6741c3e496
Aniket Rathore <aniketrathore911@gmail.com>
2025-05-06 00:16:38 +0530

    Left rotate an array by D places (Optimal Approach)

--- Demo6.java
+++ Demo6.java
@@ -5,29 +5,26 @@ public static void main(String ar[])
 {
 	int x[]={1,2,3,4,5,6,7};
 	int d=3;   
-	  rotate(x,d);
+	int n=x.length;
+  d=d%n;
+	  rotate(x,0,n-d-1);
+	  rotate(x,n-d,n-1);
+	  rotate(x,0,n-1);
 	for(int i: x)
 	{
 		System.out.print(" " +i);
 	}
 }
-public static void rotate(int x[],int d)
+public static void rotate(int x[],int s,int e)
 {
 	int n=x.length;
-	d=d%n;
-	int temp[]=new int[d];
-	for(int i=0;i<d;i++)
+	while(s<e)
 	{
-		temp[i]=x[i];
-	}
-	for(int i=d;i<n;i++)
-	{
-		x[i-d]=x[i];
-	}
-	for(int i=n-d;i<n;i++)
-	{
-		x[i]=temp[i-(n-d)];
-		// j++;
+		int temp=x[s];
+		x[s]=x[e];
+		x[e]=temp;
+		s++;
+		e--;
 	}
 }
 
