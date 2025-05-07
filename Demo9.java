14415ff01c830eb9074c8bf42bd944b8860b2844
Aniket Rathore <aniketrathore911@gmail.com>
2025-05-07 23:08:38 +0530

    Union of two Sorted Array (Optimal Approach)

--- Demo9.java
+++ Demo9.java
@@ -7,19 +7,47 @@ public static void main(String ar[])
 int arr1[]={1,1,2,3,4,5};
 int arr2[]={2,3,4,4,5,6};
 int union[]=new int[arr1.length];
-HashSet <Integer> h =new HashSet<>();
-for(int i:arr1)
-	h.add(i);
-for(int i:arr2)
-	h.add(i);
-int k=0;
-for(Integer i:h)
+int n1=arr1.length;
+int n2=arr2.length;
+int i=0,j=0,k=0;
+while(i<n1 && j<n2)
 {
-	union[k++]=i;
+	if(arr1[i]<=arr2[j])
+	{
+		if(k==0 || union[k-1]!=arr1[i])
+		{
+			union[k++]=arr1[i];
+		}
+		i++;
+	}
+	else
+	{
+		if(k==0 || union[k-1]!=arr2[j])
+		{
+			union[k++]=arr2[j];
+		}
+		j++;
+	}
 }
-for(int i=0;i<union.length;i++)
+while(i<n1)
 {
-	System.out.print(" "+union[i]);
+	if(k==0 || union[k-1]!=arr1[i])
+		{
+			union[k++]=arr1[i];
+		}
+		i++;
+}
+while(j<n2)
+{
+	if(k==0|| union[k-1]!=arr2[j])
+		{
+			union[k++]=arr2[j];
+		}
+		j++;
+}
+for(int z=0;z<union.length;z++)
+{
+	System.out.print(" "+union[z]);
 }
 }
 }
