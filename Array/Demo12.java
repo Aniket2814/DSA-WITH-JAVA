fcb7aa76e1e69b2547130c716838853b1efda804
Aniket Rathore <aniketrathore911@gmail.com>
2025-05-11 00:25:57 +0530

    Swap Alternate in Array

--- Array/Demo12.java
+++ Array/Demo12.java
@@ -1,8 +1,41 @@
+//Swap alternate in an Array
+import java.util.*;
 class Demo12
 {
 public static void main(String ar[])
 {
-
+System.out.println("Enter Size of Array");
+Scanner sc=new Scanner(System.in);
+int size=sc.nextInt();
+int arr[]=new int[size];
+if(size==0)
+{
+	System.out.println("Enter Valid Size of Array ");
+	return;
+}
+for(int i=0;i<size;i++)
+{
+	arr[i]=sc.nextInt();
+}
+ show(arr);
+for(int i:arr)
+{
+	System.out.print(" "+i);
+}
 
 }
+public static void show(int x[])
+{
+	int n=x.length;
+	for(int i=0;i<n;i+=2)
+	{
+		if(i+1<n)
+		{
+			int t=x[i];
+			x[i]=x[i+1];
+			x[i+1]=t;
+		}
+	}
+}
+
 }
