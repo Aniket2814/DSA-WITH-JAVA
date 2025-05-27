b1bb1bc5a6a845a5b6964f338765d4fcccf3d5e9
Aniket Rathore <aniketrathore911@gmail.com>
2025-05-27 23:04:47 +0530

    Merge Two Sorted Array (LeetCode 84) (Optimal : Shell Sort)

--- Array/Demo23.java
+++ Array/Demo23.java
@@ -13,36 +13,47 @@ int n=3;
  for(int i : ans)
 	 System.out.print( " " +i);
  }
-  public static int [] merge(int[] nums1, int m, int[] nums2, int n)
-  {
- int left=m-1;
-	  int right=0;
-	  int index=0;
-	  while(left>=0 && right <n)
-	  {
-		  if(nums1[left] > nums2[right])
-		  {
-			  swap(nums1,left,nums2,right);
-		       left--;
-               right++;
-		  }
-		  else
-		  {
-            break;
-		  }
-		  
-	  }
-	Arrays.sort(nums1,0,m);
-    Arrays.sort(nums2); 
-     for (int i = 0; i < n; i++) {
-    nums1[m + i] = nums2[i];
-}
-return nums1;
-   }
+   public static int [] merge(int[] nums1, int m, int[] nums2, int n) {
+    //Shell sort
+     int len=n+m;
+     int gap=(len/2) +(len%2);
+      while(gap>0)
+        {
+         int left=0;
+         int right=left+gap;
+       while(right<len)
+        {
+        if(left<m && right>=m)
+         {
+          swap(nums1,left,nums2,right-m);
+         }
+         else if(left>=m)
+          {
+           swap(nums2,left-m,nums2,right-m);
+          }
+          else
+          {
+           swap(nums1,left,nums1,right);
+           }
+           left++;
+           right++;
+          }  
+          if(gap==1) break;
+          gap=(gap/2)+(gap%2);
+           } 
+           for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+           }
+		   return nums1;
+           }
+   
  public static void swap(int[] nums1, int i, int[] nums2, int j) {
+        if(nums1[i]>nums2[j])
+        { 
         int temp = nums1[i];
         nums1[i] = nums2[j];
         nums2[j] = temp;
-    }
-
- }
+    }}
+     }
+    
+ 
