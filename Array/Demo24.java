7ae3ebc74b26ad85d5c4762f8ace639132602919
Aniket Rathore <aniketrathore911@gmail.com>
2025-05-29 00:01:00 +0530

    Maximum Product Subarray (LeetCode 154) (Better Approach)

--- Array/Demo24.java
+++ Array/Demo24.java
@@ -8,14 +8,16 @@
 	System.out.print("The maximum product subarray is: "+answer);
  }
  static int maxProductSubArray(int arr[]) {
-	    int result = Integer.MIN_VALUE;
+	    int result = arr[0];
 	    for(int i=0;i<arr.length-1;i++) 
-	        for(int j=i+1;j<arr.length;j++) {
-	            int prod = 1;
-	            for(int k=i;k<=j;k++) 
-	                prod *= arr[k];
+		{
+	            int prod = arr[i];
+	        for(int j=i+1;j<arr.length;j++) { 
 	            result = Math.max(result,prod);
+	                prod *= arr[j];
 	        }
+	            result = Math.max(result,prod);
+		}	
 	   return result;
  }
  }
