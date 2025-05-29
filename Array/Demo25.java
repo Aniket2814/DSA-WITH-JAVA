939cf7711cf3076ffd1ce167a2e7dac4550b048b
Aniket Rathore <aniketrathore911@gmail.com>
2025-05-29 23:45:06 +0530

    Print any Given Row(Pascal Traingle) (Optimal)

--- Array/Demo25.java
+++ Array/Demo25.java
@@ -12,22 +12,25 @@ public static void main(String ar[])
     pascalTriangle(row);
 	// System.out.println("The element at Column = " +col+ " and Row = " +row+ " = " +ans);
 }
-public static long nCr(int n,int row)
-{
-	long res=1;
-	for(int i=0;i<row;i++)
-	{
-		res=res*(n-i);
-		res=res/(i+1);
-	}
-	return res;
-}
+// public static long nCr(int n,int row)
+// {
+	// long res=1;
+	// for(int i=0;i<row;i++)
+	// {
+		// res=res*(n-i);
+		// res=res/(i+1);
+	// }
+	// return res;
+// }
 public static void pascalTriangle(int n)
 {
-	for(int c=1;c<=n;c++)
+	int ans=1;
+	System.out.print(" " +ans);
+	for(int c=1;c<n;c++)
 	{
-	int element=(int)nCr(n-1,c-1);
-	System.out.print(" "+element);
+	ans=ans*(n-c);
+	ans=ans/(c);
+	System.out.print(" "+ans);
 }}
 }
 
