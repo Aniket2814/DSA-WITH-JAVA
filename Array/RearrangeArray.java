c221b0a313b328d2b528098d604e13a571f51b01
Aniket Rathore <aniketrathore911@gmail.com>
2025-06-01 12:39:49 +0530

    (CodeStudio) Rearrange Array Elements by Sign (Variety 2)

--- Array/RearrangeArray.java
+++ Array/RearrangeArray.java
@@ -1,10 +1,11 @@
 // (LeetCode 2149) Rearrange Array Elements by Sign
-//Variety 1: No.of Negative and Positive are equals
+//Variety 2: No.of Negative and Positive are not equals
+import java.util.*;
 class RearrangeArray
 {
    public static void main(String ar[])
 	{
-	 int arr[]={3,1,-2,-5,2,-4};
+	 int arr[]={3,1,-2,-5,-2,-4};
 	 int ans[]=rearrangeArray(arr);
 	 System.out.println("After RearrangeArray Element by Sign");
 	 for(int i:ans)
@@ -14,23 +15,33 @@ class RearrangeArray
 	}
    public static int[] rearrangeArray(int[] nums)
     { 
-	 int n=nums.length;
-	  int pos=0;
-      int neg=1;
+	  int n=nums.length;
+	  ArrayList <Integer> pos=new ArrayList<>();
+	  ArrayList <Integer> neg=new ArrayList<>();
 	  int ans[]=new int[n];
-      for(int i=0;i<n;i++)
+	   for(int i=0;i<n;i++)
+	   {
+		   if(nums[i]>0)
+			 pos.add(nums[i]);
+	        else
+			 neg.add(nums[i]);
+	   }
+	   int k=0;
+	   int min=Math.min(pos.size(),neg.size());
+        for(int i=0;i<min;i++)
 		{
-		if(nums[i]>0)
-        {
-        ans[pos]=nums[i];
-         pos+=2; 
-        }
-        else
-        {
-         ans[neg]=nums[i];
-         neg+=2;
-        }
-		}	
+			ans[k++]=pos.get(i);
+			ans[k++]=neg.get(i);
+		}
+		  for(int i=min;i<pos.size();i++)
+		  {
+			  ans[k++]=pos.get(i);
+		  }
+	     for(int i=min;i<neg.size();i++)
+		  {
+			  ans[k++]=neg.get(i);
+		  }
+					
         return ans;		
     }
 }
